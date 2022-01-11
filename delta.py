12e43cc7dd39f62b2fc0585cfab3d9816a5559c8
Brognor <42617332+Brognor@users.noreply.github.com>
2022-01-11 15:50:26 +0100

    Add files via upload

---
+++ delta.py
@@ -0,0 +1,17 @@
+import Numerov as nu
+import matplotlib.pyplot as plt
+
+grid, step = nu.spatial_objects.uniform_grid(-60,3,3*10**6,retstep=True)
+grid=nu.spatial_objects.logarithmic_grid(grid)
+plt.rcParams['text.usetex'] = True
+
+for a in range(1,17,1):
+    a=1/a
+    
+    delta=nu.potentials.dirac_delta_smeared(grid,a)
+    plt.plot(grid,delta,label=f'a={a}')
+
+plt.xlim(0,0.4)
+plt.title(r'$\delta(r)$ representations in function of a')
+plt.legend()
+plt.savefig("pictures/delta/delta.png")
